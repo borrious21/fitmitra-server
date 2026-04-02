7cc73e59f1e4b28e036dbc910b3993ab01f46554
Parikshit Gautam <parikshit10gautam@gmail.com>
2026-04-02 19:58:39 +0545

    updated url

--- src/config/cors.config.js
+++ src/config/cors.config.js
@@ -1,7 +1,8 @@
 const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:5173",
-   "https://fitmitra-fyp.netlify.app",
+  "https://fitmitra-fyp.netlify.app",
+  "https://fitmitra-client-ev2g.vercel.app",
   ...(process.env.FRONTEND_URL ? [process.env.FRONTEND_URL] : []),
 ];
 
